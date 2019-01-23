4c60e1e96460bad1ffe860921d7cc5b5af6a6d7c
Shalain DeRamus <shalainderamus@PSADMINs-MacBook-Air-6.local>
2019-01-23 12:36:04 -0500

    added hello

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+Users = require("../models/User")
+
+const userController = {
+    index: (req, res) => {
+            res.send("Hello World *eyeroll emoji*")
+        }
+    }
+    
+module.exports = userController
